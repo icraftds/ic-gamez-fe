9585d58b3d8c9e68d867190a11ae43d8b0b641d9
rzranz <rzranz07@gmail.com>
2026-09-07 17:42:10 +0700

    fix: make leaderboard table visible on its dedicated page

--- src/views/LeaderboardView.vue
+++ src/views/LeaderboardView.vue
@@ -10,9 +10,18 @@
 </template>
 
 <script setup>
+import { onMounted } from 'vue'
 import BackgroundEffects from '../components/common/BackgroundEffects.vue'
 import HomeNavbar from '../components/home/HomeNavbar.vue'
 import LeaderboardTable from '../components/home/LeaderboardTable.vue'
+
+onMounted(() => {
+  // Trigger reveal animation instantly for standalone page
+  setTimeout(() => {
+    const reveals = document.querySelectorAll('.reveal')
+    reveals.forEach((el) => el.classList.add('active'))
+  }, 100)
+})
 </script>
 
 <style scoped>
